2cbc5ed78ecb2e9048b5069687f26fee8f63100a
Tushar Kapila <tgkprog@gmail.com>
2015-10-21 02:57:40 +0530

    unit test cases working

--- Q1/README.md
+++ Q1/README.md
@@ -6,6 +6,6 @@ Filters and transforms Products to add price and recommendataions based on categ
 See shoes.m.RecommendRunner (Main class)
 
 Sample args (these files are in this repo) :
---local --input o1/shoes-products.csv --inRCats o1/recomdsCats.csv --inPrice o1/price.csv --debug true
+--local --input o1/shoes-products.csv --inRCats o1/recomdsCats.csv --inPrice o1/price.csv --debug true  --output o1/pr-podcustAfterJoin3.csv
 
 In Eclipse Scala, choose Scala runtime and compiler 2.10 and Java run time 7
--- Q1/runSample.bat
+++ Q1/runSample.bat
@@ -1,9 +1,8 @@
-rem 
-java -cp target/Q1-1-jar-with-dependencies.jar -Xmx1G shoes.m.RecommendRunner  --local --input o1/shoes-products.csv --inRCats o1/recomdsCats.csv --inPrice o1/price.csv --debug true
+rem java -cp target/Q1-1-jar-with-dependencies.jar -Xmx1G shoes.m.RecommendRunner  --local --input o1/shoes-products.csv --inRCats o1/recomdsCats.csv --inPrice o1/price.csv --debug true
 
 
 
 
-java -cp target/Q1-1.jar;..\run\libs\*;..\run\libs\ -Xmx1G shoes.m.RecommendRunner  --local --input o1/shoes-products.csv --inRCats o1/recomdsCats.csv --inPrice o1/price.csv --debug true
+java -cp target/Q1-1.jar;..\run\libs\*;..\run\libs\ -Xmx1G shoes.m.RecommendRunner  --local --input o1/shoes-products.csv --inRCats o1/recomdsCats.csv --inPrice o1/price.csv --debug true  --output o1/pr-podcustAfterJoin3.csv
 
 
